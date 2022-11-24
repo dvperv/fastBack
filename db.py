e75a35c2b16de9c42ec6bba85c371b19e6c9bfa4
Dmitry Pervoukhin <dvperv@gmail.com>
2022-11-24 18:52:26 +0300

    Initial commit

---
+++ db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+engine = create_engine("postgresql://postgres:postgrespw@localhost:55000", echo=True, future=True)
+
+class Foo(Base):
+    __tablename__ = "foo"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return f"Foo(id={self.id!r}, name={self.name!r})"
+
+GENERATE_SCHEMA = False
+
+if GENERATE_SCHEMA:
+    print("INFO: Generating schema...")
+    Base.metadata.create_all(engine)
